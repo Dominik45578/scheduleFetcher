592c0983f0c9a2dbd09bcdf07aaa0899f7ba56aa
Dominik <dkkd3046@gmail.com>
2025-10-05 22:56:29 +0200

    Features: new endpoint

--- auth/src/main/java/com/polibuda/auth/rest/FilteredEventController.java
+++ auth/src/main/java/com/polibuda/auth/rest/FilteredEventController.java
@@ -1,10 +1,15 @@
 package com.polibuda.auth.rest;
 
 import com.polibuda.auth.dto.FilterRequestDto;
+import com.polibuda.auth.model.FacultyName;
+import com.polibuda.auth.model.GroupName;
 import com.polibuda.auth.service.IcsService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -17,5 +22,13 @@ public class FilteredEventController {
     @PostMapping("/ics")
     public String filterEvents(@RequestBody FilterRequestDto request) {
         return icsService.getIcsFile(request);
+    }
+    @GetMapping("/groups")
+    public List<String> getGroups() {
+        return GroupName.getGroupNames();
+    }
+    @GetMapping("/faculty")
+    public List<String> getFaculty() {
+        return FacultyName.getFacultyNames();
     }
 }
